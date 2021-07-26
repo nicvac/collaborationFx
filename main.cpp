3809d9d52a471c9a447624cb3acc831b3e7e38ee
nicvac <nicola.vacca@gmail.com>
2021-07-26 20:43:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	float inputB = 9;
 	float v1 = f1(inputA, inputB);
 	float v2 = f2(inputA, inputB);
-	std::cout << "v1:" << v1    << std::endl;
-	std::cout << "v2:" << v2    << std::endl;
+	std::cout << "f1:" << v1    << std::endl;
+	std::cout << "f2:" << v2    << std::endl;
 	std::cout << "f3:" << v2-v1 << std::endl;
 }
